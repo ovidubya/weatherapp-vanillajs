e75b213cf3bc7ce36a4bdeba3e1727784110aa76
Shalom <Ovadia.Shalom@verint.com>
2019-03-07 17:26:30 -0600

    finished retrieves data, current locatation

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 ## Features for 1.0
 
 - ~~Written in JavaScript~~
-- Retrieves data from Apixu API
+- ~~Retrieves data from Apixu API~~
 - Display today's weather and the 5-day forecast
-- Uses the current location (if supported)
+- ~~Uses the current location (if supported)~~
 - Allow choosing a new location
 - Allow choosing to display in Fahrenheit or Celsius
 - Show the weather with reasonable presentation
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,12 +1,51 @@
+/**
+ * A class module that is able to make requests to the apixu API
+ */
 export class Weather {
+
+    /**
+     * Returns if we are using https
+     */
+    get isHttps() { return location.protocol == 'https:'; }
+
+    /**
+     * Return if geolocation is supported
+     */
+    get isGeo() { return !!navigator.geolocation; }
+
+    /**
+     * Set up initial currentAPI url for future requests
+     * @param {api key for the Weather object} apiKey 
+     */
     constructor(apiKey) {
         this.currentAPI = `${location.protocol}//api.apixu.com/v1/current.json?key=${apiKey}`; 
     }
+
+    /**
+     * Returns the current location if available, otherwise defaults to a hardcoded zip code
+     * navigator.geolocation isn't supported in all browsers, and it does require https and not http
+     * so first check if we are on a https protocal otherwise just do the zipcode
+     */
     getCurrentJSON() {
-        fetch(this.currentAPI + '&q=Chicago')
-        .then(data => data.json())
-        .then(result => console.log(result))
-        .catch(err => console.log(err))
+
+        if(this.isGeo && this.isHttps ) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                
+                
+                fetch(this.currentAPI + `&q=${pos.coords.latitude},${pos.coords.longitude}`)
+                    .then(data => data.json())
+                    .then(result => console.log(result))
+                    .catch(err => console.log(err));
+            });
+            
+
+        }else {
+
+            fetch(this.currentAPI + '&q=60661')
+                .then(data => data.json())
+                .then(result => console.log(result))
+                .catch(err => console.log(err));
+        }
     }
 }
 
